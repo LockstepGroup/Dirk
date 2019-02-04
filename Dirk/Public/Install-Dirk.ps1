3985837687bf9a9d030490ccc61a6dce3c4c9af0
Brian Addicks <brian@addicks.us>
2019-02-04 15:43:09 -0500

    finally fixed elevation when not needed in Install-Dirk !deploy

--- Dirk/Public/Install-Dirk.ps1
+++ Dirk/Public/Install-Dirk.ps1
@@ -50,31 +50,6 @@ function Install-Dirk {
                 } catch {
                     $PSCmdlet.ThrowTerminatingError([HelperProcessError]::throwCustomError(1000, $profile.AllUsersAllHosts))
                 }
-            } else {
-                # only update $profile.AllUsersAllHosts if $env:DirkRoot is not already correct
-                switch -Regex (Get-OsVersion) {
-                    'MacOS' {
-                        Write-Verbose "$VerbosePrefix OS: MacOS: Adding `$env:DirkRoot to `$profile.AllUsersAllHosts"
-                        if ($EnvIsSet) {
-                            $FullContent = (Get-Content $profile.AllUsersAllHosts) -replace $EnvIsSetRx, $LineToAdd
-                            Write-Output $FullContent | sudo tee $profile.AllUsersAllHosts > /dev/null
-                        } else {
-                            Write-Output $LineToAdd | sudo tee -a $profile.AllUsersAllHosts > /dev/null
-                        }
-
-                    }
-                    'Windows' {
-                        Write-Verbose "$VerbosePrefix OS: Windows: Adding `$env:DirkRoot to `$profile.AllUsersAllHosts"
-                        if ($EnvIsSet) {
-                            $Command = "(Get-Content `$profile.AllUsersAllHosts) -replace '$EnvIsSetRx','$LineToAdd' | Set-Content `$profile.AllUsersAllHosts"
-                        } else {
-                            $Command = "Add-Content -Path `$profile.AllUsersAllHosts -Value '$LineToAdd'"
-                        }
-                        $Bytes = [System.Text.Encoding]::Unicode.GetBytes($Command)
-                        $EncodedCommand = [Convert]::ToBase64String($Bytes)
-                        Invoke-ElevatedProcess -Arguments "-EncodedCommand $encodedCommand" | Out-Null
-                    }
-                }
             }
         }
 
@@ -82,8 +57,8 @@ function Install-Dirk {
         Write-Verbose "$VerbosePrefix setting `$env:DirkRoot to $ResolvedPath"
         $env:DirkRoot = $ResolvedPath
 
-        <# # set permanently
-        if ($LineToAdd -ne $EnvIsSet) {
+        # set permanently
+        if ($LineToAdd -ne $EnvIsSet.Line) {
             # only update $profile.AllUsersAllHosts if $env:DirkRoot is not already correct
             switch -Regex (Get-OsVersion) {
                 'MacOS' {
@@ -108,7 +83,7 @@ function Install-Dirk {
                     Invoke-ElevatedProcess -Arguments "-EncodedCommand $encodedCommand" | Out-Null
                 }
             }
-        } #>
+        }
 
 
         ###########################################################################
